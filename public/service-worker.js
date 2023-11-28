c5cdc74a37c4ce94f1386a37b9d9a6c7e450ce53
Nguyen Thi Bich Kieu <109791045+kieu1004@users.noreply.github.com>
2023-11-28 10:53:36 +0700

    refactor: done sw

---
+++ public/service-worker.js
@@ -0,0 +1,37 @@
+// @ts-ignore
+const CACHE_NAME = "version-1"
+// @ts-ignore
+const urlsToCache = ["index.html"]
+
+self.addEventListener('install', (event) => {
+  // @ts-ignore
+  event.waitUntil(
+    // @ts-ignore
+    caches.open(CACHE_NAME).then((cache) => {
+      cache.addAll([
+        'assets/clip.png',
+        'assets/more.png',
+        'assets/check.png',
+        'start-152x152.png',
+        'manifest.json',
+        'dragDrop.js',
+        'dragDropTouchEvent.js',
+        'index.html',
+      ])
+    })
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  // @ts-ignore
+  event.respondWith(
+    // @ts-ignore
+    caches.match(event.request).then((resq) => {
+      if (resq) {
+        return resq;
+      }
+      // @ts-ignore
+      return fetch(event.request);
+    })
+  );
+});
